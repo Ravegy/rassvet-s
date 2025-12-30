5d03a70acdcf17c1645414d0c37651be668b2df1
Ravegy <vladislav.zip.parts@yandex.ru>
2025-12-30 15:29:11 +0300

    Update products.js

--- products.js
+++ products.js
@@ -30,7 +30,240 @@ const productsData = [
         "category": "Двигатель",
         "price": 42100,
         "image": "pump.jpg"
+    },
+    {
+        "id": 5,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 6,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 7,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 8,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 9,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 10,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 11,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 12,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 13,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 14,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 15,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 16,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 17,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 18,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 19,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 20,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 21,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 22,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 23,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 24,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 25,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 26,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 27,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 28,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 29,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 30,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 31,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 32,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
+    },
+    {
+        "id": 33,
+        "article": "CAT-550-F",
+        "name": "Топливный насос CAT",
+        "category": "Двигатель",
+        "price": 42100,
+        "image": "pump.jpg"
     }
 ];
+
 
 export default productsData;

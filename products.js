c1b917076e876088b11e17a36e4014373894a1ec
Ravegy <vladislav.zip.parts@yandex.ru>
2025-12-30 14:33:33 +0300

    Update products.js

--- products.js
+++ products.js
@@ -9,18 +9,11 @@
     },
     {
         "id": 2,
-        "article": "PN-BC-404",
-        "name": "Пильная цепь Oregon",
-        "category": "Расходные материалы",
-        "price": 4500,
-        "image": "chain.jpg"
-    },
-    {
-        "id": 3,
         "article": "VL-990-TR",
         "name": "Фильтр масляный Volvo",
         "category": "Фильтры",
         "price": 3200,
         "image": "filter.jpg"
     }
+
 ]

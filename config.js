de094e68971337884acd9bcb4f62c84128f6685c
Ravegy <vladislav.zip.parts@yandex.ru>
2026-01-05 03:35:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,13 @@ const SITE_CONFIG = {
     phone: "79818881337",
     displayPhone: "+7 (981) 888-13-37",
     address: "📍 г. Санкт-Петербург",
-    // Ссылка на вашу Google Таблицу (CSV)
     sheetUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSWW1kw6De7LtGdpg_wFUyJBWeapw_WtiaRZmmwreIFphLg6W_xv-ThZJL6_OmxIUN0U8sNGSiPpAa3/pub?output=csv',
-    // Ссылка на заглушку, если фото товара нет
     placeholderImage: 'https://placehold.co/400x300?text=Komatsu',
-    // Текст сообщения для WhatsApp по умолчанию
-    waDefaultMessage: 'Здравствуйте! Меня интересует запчасть: '
+    waDefaultMessage: 'Здравствуйте! Меня интересует запчасть: ',
+    // Основные цвета в стиле Sever-Filters
+    colors: {
+        primary: "#004a99", // Синий
+        accent: "#e31e24",  // Красный (для важных кнопок)
+        dark: "#1a1a1a"     // Темный фон шапки
+    }
 };

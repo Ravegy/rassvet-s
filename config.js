068907b8ff139f5189c6cc5c7702a312582ffecc
Ravegy <vladislav.zip.parts@yandex.ru>
2026-01-05 07:39:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
 const SITE_CONFIG = {
+    // Основные данные компании
     companyName: "РАССВЕТ-С",
-    phone: "79818881337",
+    phone: "79818881337", 
     displayPhone: "+7 (981) 888-13-37",
-    address: "г. Санкт-Петербург",
+    email: "info@rassvet-s.ru", // Добавили почту
+    address: "г. Санкт-Петербург, ул. Промышленная, д. 42",
     workTime: "Пн-Пт: 09:00 - 18:00",
+    
+    // Ссылка на твою Гугл Таблицу (CSV)
     sheetUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSWW1kw6De7LtGdpg_wFUyJBWeapw_WtiaRZmmwreIFphLg6W_xv-ThZJL6_OmxIUN0U8sNGSiPpAa3/pub?output=csv',
+    
+    // Картинка-заглушка (если фото нет)
     placeholderImage: 'https://s6.iimage.su/s/05/g6QcWpxxrKi4shjWNuvvHO7BcPELg73RviDN0SNH.png',
-    waDefaultMessage: 'Здравствуйте! Меня интересует запчасть: '
+    
+    // Сообщение для WhatsApp
+    waDefaultMessage: 'Здравствуйте! Меня интересует запчасть: ',
+
+    // Настройки каталога
+    itemsPerPage: 12, // Показывать по 12 товаров (3 ряда по 4 шт)
+    cacheTime: 60 // Время хранения каталога в памяти браузера (в минутах)
 };

ba374d19e3345e6e487df2a3e05b6ffd5823cbad
Ravegy <vladislav.zip.parts@yandex.ru>
2026-01-05 09:36:11 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const SITE_CONFIG = {
     workTime: "Пн-Пт: 09:00 - 18:00",
     
     // Ссылка на Гугл Таблицу (CSV)
-    sheetUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSWW1kw6De7LtGdpg_wFUyJBWeapw_WtiaRZmmwreIFphLg6W_xv-ThZJL6_OmxIUN0U8sNGSiPpAa3/pub?output=csv',
+    sheetUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSWW1kw6De7LtGdpg_wFUyJBWeapw_WtiaRZmmwreIFphLg6W_xv-ThZJL6_OmxIUN0U8sNGSiPpAa3/pub?gid=1023442601&single=true&output=csv',
     
     // Картинка-заглушка (если нет фото)
     placeholderImage: 'https://s6.iimage.su/s/05/g6QcWpxxrKi4shjWNuvvHO7BcPELg73RviDN0SNH.png',

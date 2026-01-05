33741606a7f56a3944a84607bf90692404237863
Ravegy <vladislav.zip.parts@yandex.ru>
2026-01-05 06:11:24 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const SITE_CONFIG = {
     address: "г. Санкт-Петербург",
     workTime: "Пн-Пт: 09:00 - 18:00",
     sheetUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSWW1kw6De7LtGdpg_wFUyJBWeapw_WtiaRZmmwreIFphLg6W_xv-ThZJL6_OmxIUN0U8sNGSiPpAa3/pub?output=csv',
-    placeholderImage: 'https://downloader.disk.yandex.ru/preview/356b824003166ed4522881b17ad551f0c8c90d9314c68c397f27c3b190d1bb03/695b6290/T5WCFgVrs6nrxI74sTiyjdevid1_rD133jRx_02FFiy4pr_ETaF6Q3UUJ2yRo7VhirArSjBxeLjSONA8573hrw%3D%3D?uid=0&filename=photo_2025-12-30_06-49-09.webp&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v3&size=1920x960',
+    placeholderImage: 'https://s6.iimage.su/s/05/g6QcWpxxrKi4shjWNuvvHO7BcPELg73RviDN0SNH.png',
     waDefaultMessage: 'Здравствуйте! Меня интересует запчасть: '
 };
